3abc20d4446305f3540eb2f7ced53ce2e09470f5
Aral Acikalin <aralacikalin@hotmail.com>
2026-06-27 12:46:42 +0300

    use character resource to load moves instead of hard coding it

--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var player_moves: Array[DanceMove]
+@export var default_player_character: Character
 
 # will be used when we we save the game and load it back in, to restore the player's moves
 var saved_data = null
@@ -14,7 +15,8 @@ func load_player_moves() -> void:
 		#reload from saved data
 		return
 	
-	for move in player_moves:
+	for move in default_player_character.dance_moves:
+		#TODO: i think we can eleminate PlayerMoves global, and store all the moves in a character node script, then ui can use the character node to get and show the moves
 		PlayerMoves.add(move)
 
 
