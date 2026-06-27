113e8a7780d42e82d2b3a7b3c661d98fb8f68ebe
Aral Acikalin <aralacikalin@hotmail.com>
2026-06-27 13:25:46 +0300

    add proposal for managers setup

--- worlds/fight_world.gd
+++ worlds/fight_world.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var fight_manager = $FightManager
 
 func _ready():
+	#TODO: probably can move to game manager currently when all scenes have their own setup everywhere it makes it so hard to track where something is set
 	fight_manager.whose_turn_changed.connect(ui.swap_sides)
 	fight_manager.turn_count_changed.connect(ui.set_turn)
 	ui.move_performed.connect(fight_manager.select_move)
